316245202b82492c731c385b16e03e3f4afa480e
it21295638 Vishvanath <99311565+it21295638@users.noreply.github.com>
2022-05-20 16:47:22 +0000

    submission done

---
+++ Feedback.cpp
@@ -0,0 +1,10 @@
+Feedback::Feedback(int fbNo, string mail, string fb) 
+{ 
+feedbackNo = fbNo; email = mail; 
+feedbackmsg = fb;} 
+void Feedback::displayFeedback() 
+{ 
+} 
+Feedback::~Feedback() 
+{ 
+} 
---
+++ Feedback.h
@@ -0,0 +1,11 @@
+class Feedback 
+{ 
+private: 
+int feedbackNo; 
+string email; 
+string feedbackmsg; 
+public: 
+Feedback(int fbNo, string mail, string fb); 
+void displayFeedback(); 
+~Feedback(); 
+}; 
